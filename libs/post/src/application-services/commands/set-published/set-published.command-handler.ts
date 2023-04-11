248b51873ee813744beccfefc375ffc7bef04849
Maryshev Andrey <a.maryshev@iqdev.digital>
2023-04-11 21:42:09 +0500

    feat: set published post command

---
+++ libs/post/src/application-services/commands/set-published/set-published.command-handler.ts
@@ -0,0 +1,26 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { SetPublishedCommand } from '@lib/post/application-services/commands/set-published/set-published.command';
+import { PostAggregate } from '@lib/post';
+import { PostRepository } from '@lib/post/providers';
+import { BadRequestException, Logger } from '@nestjs/common';
+
+@CommandHandler(SetPublishedCommand)
+export class SetPublishedCommandHandler
+  implements ICommandHandler<SetPublishedCommand, PostAggregate>
+{
+  private readonly logger = new Logger(SetPublishedCommandHandler.name);
+  constructor(private readonly postRepository: PostRepository) {}
+  async execute({ id }: SetPublishedCommand): Promise<PostAggregate> {
+    const existPost = await this.postRepository.findOne(id).catch((err) => {
+      this.logger.error(err);
+      return null as PostAggregate;
+    });
+    if (!existPost) {
+      throw new BadRequestException(`Post by id ${id} not found!`);
+    }
+    const postAggregate = PostAggregate.create(existPost);
+    await postAggregate.setPublished();
+    await this.postRepository.save(postAggregate);
+    return postAggregate;
+  }
+}
