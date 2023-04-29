d5abce902e4af7d2cf882cd3df3e49fb80d31cb5
Maryshev Andrey <a.maryshev@iqdev.digital>
2023-04-29 17:24:36 +0500

    ADD: jwt strategy

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtFactory } from '@lib/auth/config';
+import { jwtFactory } from './config';
 import { GUARDS } from '@lib/auth/guards';
+import { STRATEGIES } from '@lib/auth/strategies';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync(jwtFactory),
   ],
-  providers: [AuthService, ...GUARDS],
+  providers: [AuthService, ...GUARDS, ...STRATEGIES],
   exports: [AuthService],
 })
 export class AuthModule {}
