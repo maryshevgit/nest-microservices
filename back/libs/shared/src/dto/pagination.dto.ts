305fb8b8763a5ecde21f2c58896e17b29e5aeed0
Maryshev Andrey <a.maryshev@iqdev.digital>
2023-04-30 15:37:07 +0500

    REFACTOR: moved nest in folder back

---
+++ back/libs/shared/src/dto/pagination.dto.ts
@@ -0,0 +1,23 @@
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
+import { ArgsType, Field, Int } from '@nestjs/graphql';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+@ArgsType()
+export class PaginationDto {
+  @ApiPropertyOptional({ description: 'Пропуск строк', type: 'number' })
+  @IsOptional()
+  @Min(0)
+  @IsNumber({ allowNaN: false, allowInfinity: false })
+  @Type(() => Number)
+  @Field(() => Int, { description: 'Пропуск строк' })
+  offset = 0;
+
+  @ApiPropertyOptional({ description: 'Количество строк', type: 'number' })
+  @IsOptional()
+  @IsNumber({ allowNaN: false, allowInfinity: false })
+  @Type(() => Number)
+  @IsPositive()
+  @Field(() => Int, { description: 'Количество строк' })
+  limit = 15;
+}
---
+++ back/src/api/resolvers/post/post.resolver.ts
@@ -0,0 +1,28 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { PostFacade } from '@lib/post/application-services';
+import { PaginationDto } from '@lib/shared';
+import { plainToInstance } from 'class-transformer';
+import { PaginatedPosts, PostResponse } from '../responses';
+
+@Resolver(() => PostResponse)
+export class PostResolver {
+  constructor(private readonly postFacade: PostFacade) {}
+
+  @Query(() => PostResponse, { name: 'post' })
+  async getPostById(@Args('id') id: string) {
+    return this.postFacade.queries.getOnePost(id);
+  }
+
+  @Query(() => PaginatedPosts, { name: 'posts' })
+  async getPosts(@Args() paginationDto: PaginationDto) {
+    const pagination = plainToInstance(PaginationDto, paginationDto);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const [data, total] = await this.postFacade.queries.getAllPosts(pagination);
+    return {
+      ...pagination,
+      data,
+      total,
+    };
+  }
+}
