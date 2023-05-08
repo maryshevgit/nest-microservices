51750609685b917dab504820bf1aa55230454267
Maryshev Andrey <a.maryshev@iqdev.digital>
2023-05-08 13:41:06 +0500

    add: error boundary provider

--- front/app/[lng]/layout.tsx
+++ front/app/[lng]/layout.tsx
@@ -7,6 +7,7 @@ import { Theme } from '@/shared/const/theme';
 import '@/app/styles/globals.css';
 import LayoutPage from './layout-page';
 import { useTranslation } from '../i18n';
+import { ErrorBoundary } from '@/app/providers/error-boundary';
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
@@ -33,11 +34,13 @@ export default async function RootLayout({
         <title>{t('title')}</title>
       </Head>
       <body>
-        <ThemeProvider initialTheme={Theme.LIGHT}>
-          <LayoutPage lng={lng}>
-            {children}
-          </LayoutPage>
-        </ThemeProvider>
+        <ErrorBoundary lng={lng}>
+          <ThemeProvider initialTheme={Theme.LIGHT}>
+            <LayoutPage lng={lng}>
+              {children}
+            </LayoutPage>
+          </ThemeProvider>
+        </ErrorBoundary>
       </body>
     </html>
   );
---
+++ front/src/app/providers/error-boundary/ui/error-boundary.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React, { ErrorInfo, ReactNode, Suspense } from 'react';
+import { ErrorPage } from '@/widgets/error-page';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+  lng: string
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary
+  extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children, lng } = this.props;
+
+    if (hasError) {
+      // You can render any custom fallback UI
+      return (
+        <Suspense fallback="">
+          <ErrorPage lng={lng} />
+        </Suspense>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
