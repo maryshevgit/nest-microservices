20cdfd68062495a8d34856dbe13642aca2bf346a
Maryshev Andrey <a.maryshev@iqdev.digital>
2023-05-10 18:41:57 +0500

    add: gql guard

--- back/src/api/resolvers/post/post.resolver.ts
+++ back/src/api/resolvers/post/post.resolver.ts
@@ -4,17 +4,22 @@ import { PaginationDto } from '@lib/shared';
 import { plainToInstance } from 'class-transformer';
 import { PaginatedPosts, PostResponse } from '../responses';
 import { CreatePostInput, UpdatePostInput } from '../inputs';
-import { randomUUID } from 'crypto';
+import { GqlCurrentUser, ICurrentUser, Public } from '@lib/auth';
+import { GqlGuard } from '@lib/auth/guards/gql.guard';
+import { UseGuards } from '@nestjs/common';
 
+@UseGuards(GqlGuard)
 @Resolver(() => PostResponse)
 export class PostResolver {
   constructor(private readonly postFacade: PostFacade) {}
 
+  @Public()
   @Query(() => PostResponse, { name: 'post' })
   async getPostById(@Args('id') id: string) {
     return this.postFacade.queries.getOnePost(id);
   }
 
+  @Public()
   @Query(() => PaginatedPosts, { name: 'posts' })
   async getPosts(@Args() paginationDto: PaginationDto) {
     const pagination = plainToInstance(PaginationDto, paginationDto);
@@ -29,18 +34,24 @@ export class PostResolver {
   }
 
   @Mutation(() => PostResponse)
-  async createPost(@Args('createPostInput') createPostInput: CreatePostInput) {
+  async createPost(
+    @GqlCurrentUser() currentUser: ICurrentUser,
+    @Args('createPostInput') createPostInput: CreatePostInput,
+  ) {
     return this.postFacade.commands.createPost({
       ...createPostInput,
-      authorId: randomUUID(),
+      authorId: currentUser.userId,
     });
   }
 
   @Mutation(() => PostResponse)
-  async updatePost(@Args('updatePostInput') updatePostInput: UpdatePostInput) {
+  async updatePost(
+    @GqlCurrentUser() currentUser: ICurrentUser,
+    @Args('updatePostInput') updatePostInput: UpdatePostInput,
+  ) {
     return this.postFacade.commands.updatePost({
       ...updatePostInput,
-      authorId: randomUUID(),
+      authorId: currentUser.userId,
     });
   }
 
