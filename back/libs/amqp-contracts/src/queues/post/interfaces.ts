723ebcb2883465e8230407c60ae1256aea4ccfad
Maryshev Andrey <a.maryshev@iqdev.digital>
2023-06-06 18:18:36 +0500

    add: amqp-contracts, create post contract

---
+++ back/libs/amqp-contracts/src/queues/post/interfaces.ts
@@ -0,0 +1,13 @@
+export interface CreatePostRequest {
+  title: string;
+  message: string;
+}
+
+export interface PostResponse {
+  id: string;
+  title: string;
+  message: string;
+  authorId: string;
+  createdAt: string;
+  updatedAt: string;
+}
